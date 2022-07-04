ca9946ed7f00f8764490a8c30f6cb0fda65bc6cb
Droolworthy <107348019+Droolworthy@users.noreply.github.com>
2022-07-04 13:34:15 +0300

    Update Program.cs

--- CS19/Program.cs
+++ CS19/Program.cs
@@ -6,52 +6,54 @@ namespace CS19
     {
         static void Main(string[] args)
         {
-            Random random = new Random(); //ранодом
-            string userInput; //ввод
-            int Nero = 100; // охотник на демонов Неро, 100 хп
-            int stinger = 100;// жало, 100 урона
-            int highSide = 50; // подбросить врага вверх, 50 урона
-            int bossMundus = 500; // босс, 500 здоровья
-            int bossDamage = 20; // урон от босса, 20 ед
-            int death = 0; //смерть
-            int devilTrigger = 250; // дьявольский триггер, 250 урона
-            int medicalPackage = 25; // хил, восстанавливает 25 здоровья
-            int recoveryBossMundus = 50; //хил демона 
-            int bleeding = 50; //кровотечение
-            int probabilityActivationDevilTrigger = 50; //вероятность активации девил триггера
-            int emptyMedicalBag = 0; //пустой мед пакет
-            int maxHealthBossMundus = 550; // макс запаз жизей босса
-            int maxHealthNero = 125; // максимальный запас Неро
-            bool stingerEffect = false; //активация клинка Жало
-            bool exitApp = true; // булевое выражение, выход из игры
+            Random random = new Random(); 
+            string userInput; 
+            int neroHealth = 100;  
+            int stingerDamage = 100;
+            int highSideDamage = 50; 
+            int demonHealth = 500; 
+            int bossDamage = 20; 
+            int lethalAmountHealth = 0; 
+            int devilTriggerDamage = 250; 
+            int medicalPackageHealth = 25; 
+            int recoveryHealthBossMundus = 50; 
+            int bleedingDamage = 50; 
+            int activationDevilTrigger = 50;
+            int minimumActivationDevilTrigger = 1;
+            int maximumActivationDevilTrigger = 100;
+            int emptyMedicalBagHealth = 0; 
+            int maxHealthBossMundus = 550; 
+            int maxHealthNero = 125; 
+            bool canStingerEffect = false; 
+            bool canExitApp = true; 
 
             Console.WriteLine("Вы охотник на демонов Неро, перед вами главный демон игры Мундус. Начнём!");
             Console.WriteLine();
-            Console.WriteLine($"У босса {bossMundus} хп");
-            Console.WriteLine($"У вас {Nero} хп");
+            Console.WriteLine($"У босса {demonHealth} хп");
+            Console.WriteLine($"У вас {neroHealth} хп");
             Console.WriteLine();
-            Console.WriteLine($"1 - Жало. Острый клинок, который активируется в воздухе, нанося {stinger} урона.");
-            Console.WriteLine($"2 - Подбросить врага вверх. Способность Неро наносящая {highSide} урона. " +
+            Console.WriteLine($"1 - Жало. Острый клинок, который активируется в воздухе, нанося {stingerDamage} урона.");
+            Console.WriteLine($"2 - Подбросить врага вверх. Способность Неро наносящая {highSideDamage} урона. " +
                 $"Даёт возможность активировать первый скилл - Жало."); 
-            Console.WriteLine($"3 - Дьявольский триггер. Атака на {devilTrigger} едениц. " +
-                $"Риск кровотечения составляет {bleeding} процентов.");
-            Console.WriteLine($"4 - Медицинский пакет. Восстанавливает здоровье на {medicalPackage} едениц. " +
-                $"При использовании Демон восстанавливает {recoveryBossMundus} едениц.");
+            Console.WriteLine($"3 - Дьявольский триггер. Атака на {devilTriggerDamage} едениц. " +
+                $"Риск кровотечения составляет {bleedingDamage} процентов.");
+            Console.WriteLine($"4 - Медицинский пакет. Восстанавливает здоровье на {medicalPackageHealth} едениц. " +
+                $"При использовании Демон восстанавливает {recoveryHealthBossMundus} едениц.");
             Console.WriteLine("5 - Выход из игры");
             Console.WriteLine();
             Console.Write("Введите команду: ");
 
             userInput = Console.ReadLine();
 
-            while (exitApp)
+            while (canExitApp)
             {
 
-                if (Nero <= death)
+                if (neroHealth <= lethalAmountHealth)
                 {
                     Console.WriteLine(" Победа Демона! Попробуй ещё раз!");
                     break;
                 }
-                else if (bossMundus <= death)
+                else if (demonHealth <= lethalAmountHealth)
                 {
                     Console.WriteLine(" Победа Неро! Демон разлетается на тысячи кусков, отправляйся в АД!");
                     break;
@@ -60,14 +62,14 @@ namespace CS19
                 switch (userInput)
                 {
                     case "1":
-                        if (stingerEffect == true)
+                        if (canStingerEffect == true)
                         {
-                            bossMundus -= stinger;
-                            stingerEffect = false;
-                            Console.WriteLine($"Ваш клинок превращается в острое жало и взымается вверх нанося Мундусу {stinger} урона");
-                            Nero -= bossDamage;
-                            Console.WriteLine($"У босса осталось {bossMundus} хп.");
-                            Console.WriteLine($"У вас осталось {Nero} хп.");
+                            demonHealth -= stingerDamage;
+                            canStingerEffect = false;
+                            Console.WriteLine($"Ваш клинок превращается в острое жало и взымается вверх нанося Мундусу {stingerDamage} урона");
+                            neroHealth -= bossDamage;
+                            Console.WriteLine($"У босса осталось {demonHealth} хп.");
+                            Console.WriteLine($"У вас осталось {neroHealth} хп.");
                         }
                         else
                         {
@@ -77,58 +79,58 @@ namespace CS19
                         userInput = Console.ReadLine();
                         break;
                     case "2":
-                        bossMundus -= highSide;
-                        Nero -= bossDamage;
-                        stingerEffect = true;
-                        Console.WriteLine($"Вы подкинули демона вверх нанеся ему {highSide} урона.");
-                        Console.WriteLine($"У босса осталось {bossMundus} хп.");
-                        Console.WriteLine($"У вас осталось {Nero} хп.");
+                        demonHealth -= highSideDamage;
+                        neroHealth -= bossDamage;
+                        canStingerEffect = true;
+                        Console.WriteLine($"Вы подкинули демона вверх нанеся ему {highSideDamage} урона.");
+                        Console.WriteLine($"У босса осталось {demonHealth} хп.");
+                        Console.WriteLine($"У вас осталось {neroHealth} хп.");
                         Console.Write("Следующая атака под номером - ");
                         userInput = Console.ReadLine();
                         break;
                     case "3":
-                        if (probabilityActivationDevilTrigger > random.Next(1, 100))
+                        if (activationDevilTrigger > random.Next(minimumActivationDevilTrigger, maximumActivationDevilTrigger)) 
                         {
-                            bossMundus -= devilTrigger;
+                            demonHealth -= devilTriggerDamage;
                             Console.WriteLine($"Вы активируйте дьявольсий триггер и превращайтесь в Демона! " +
-                                $"Вы наносите {devilTrigger} едениц урона!");
-                            Console.WriteLine($"У босса осталось {bossMundus} хп.");
-                            Console.WriteLine($"У вас осталось {Nero} хп.");
+                                $"Вы наносите {devilTriggerDamage} едениц урона!");
+                            Console.WriteLine($"У босса осталось {demonHealth} хп.");
+                            Console.WriteLine($"У вас осталось {neroHealth} хп.");
                             Console.Write("Следующая атака под номером - ");
                         }
                         else
                         {
-                            Nero -= bleeding;
+                            neroHealth -= bleedingDamage;
                             Console.WriteLine($"Попытка провалилась! При активации превращения, " +
-                                $"у вас открылась старая рана и пошла кровь, которая нанесла вам {bleeding} едениц урона!");
-                            Console.WriteLine($"У босса осталось {bossMundus} хп.");
-                            Console.WriteLine($"У вас осталось {Nero} хп.");
+                                $"у вас открылась старая рана и пошла кровь, которая нанесла вам {bleedingDamage} едениц урона!");
+                            Console.WriteLine($"У босса осталось {demonHealth} хп.");
+                            Console.WriteLine($"У вас осталось {neroHealth} хп.");
                             Console.Write("Следующая атака под номером - ");
                         }
                         userInput = Console.ReadLine();
                         break;
                     case "4":
-                        if (Nero < maxHealthNero || bossMundus < maxHealthBossMundus)
+                        if (neroHealth < maxHealthNero || demonHealth < maxHealthBossMundus)
                         {
-                            Nero += medicalPackage;
-                            bossMundus += recoveryBossMundus;
-                            Console.WriteLine($"Вы использовали аптечку восстановив {medicalPackage} здоровья. Теперь у Неро {Nero} хп.");
-                            Console.WriteLine($"Демон не сидел не месте и решил восстановить {recoveryBossMundus} здоровья, " +
-                                $"теперь у него {bossMundus} хп.");
+                            neroHealth += medicalPackageHealth;
+                            demonHealth += recoveryHealthBossMundus;
+                            Console.WriteLine($"Вы использовали аптечку восстановив {medicalPackageHealth} здоровья. Теперь у Неро {neroHealth} хп.");
+                            Console.WriteLine($"Демон не сидел не месте и решил восстановить {recoveryHealthBossMundus} здоровья, " +
+                                $"теперь у него {demonHealth} хп.");
                             Console.Write("Следующая атака под номером - ");
                         }
                         else
                         {
-                            Nero += emptyMedicalBag;
-                            bossMundus += emptyMedicalBag;
+                            neroHealth += emptyMedicalBagHealth;
+                            demonHealth += emptyMedicalBagHealth;
                             Console.WriteLine($"Вы уже пользовались аптечкой, в ней ничего нет. " +
-                                $"Вы восстанавливайте {emptyMedicalBag} хп. Атакуйте!");
+                                $"Вы восстанавливайте {emptyMedicalBagHealth} хп. Атакуйте!");
                             Console.Write("Следующая атака под номером - ");
                         }
                         userInput = Console.ReadLine();
                         break;
                     case "5":
-                        exitApp = false;
+                        canExitApp = false;
                         break;
                     default:
                         {
